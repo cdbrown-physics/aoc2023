d8268029d6a76ef6a59e2e7573b4ae48bc3bb935
Cody Brown <cody2093@yahoo.com>
2023-12-13 22:47:25 -0700

    Working on day 3, getting a seg fault

--- day03/main.cpp
+++ day03/main.cpp
@@ -1,38 +1,55 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <map>
 #include <regex>
 
 using std::cout;
 using std::endl;
 
-int testing() 
+std::map<std::string, int> findNumbersInLine(std::string input, int offSet) 
 {
-    std::string s = "..467...114..";
+    std::vector<std::map<std::string, int>> mapEntrys;
     std::regex pattern("(\\d+)");   // matches numbers in a string
-    std::smatch match;   // for returned details of the match
-    std::sregex_token_iterator it (s.begin(), s.end(), pattern);
+
+    std::sregex_token_iterator it (input.begin(), input.end(), pattern);
     std::sregex_token_iterator end;
     std::vector<std::string> foo(it, end);
+
+    mapEntrys.reserve(foo.size());
     for (auto f: foo)
     {
+        std::map<std::string, int> mapEntry;
         cout << f << endl;
         // Get the position of those sub string
-        size_t pos = s.find(f);
+        size_t pos = input.find(f);
         cout << "found at: " << pos << " - " << pos + f.size() -1 << endl;
+        // Turn that possition into a string map key
+        std::string mapKey = "";
+        int posInt = static_cast<int>(pos);
+        for(int i = pos; i < pos + f.size(); i++)
+        {
+            mapKey += std::to_string(i) + ",";
+        }
+        cout << "This is the mapKey: " << mapKey << endl;
+        mapEntry[mapKey] = stoi(f);
+        cout << "Trying to push back" << endl;
+        mapEntrys.push_back(mapEntry);
     }
-    return 0;
-}
 
+}
 
 int main(int argc, char* argv[])
 {
     std::string line_input = "";
     std::ifstream MyReadFile(argv[1]);
+    int offSet = 0;
     while(std::getline (MyReadFile, line_input))
     {
-        cout << line_input << endl;
+        int size = line_input.size();
+        cout << line_input << "size" << size << endl;
+        findNumbersInLine(line_input, offSet);
+        offSet += line_input.size();
     }
-    testing();
     return 0;
 }
