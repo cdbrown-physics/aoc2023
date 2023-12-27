5fae717ae4d7cfafd1f26b89b8f96d1dfbb80042
Cody Brown <cody2092@yahoo.com>
2023-12-26 21:48:24 -0700

    Minor refactoring to just make a map for the one line of input text

--- day03/main.cpp
+++ day03/main.cpp
@@ -7,36 +7,39 @@
 using std::cout;
 using std::endl;
 
-std::map<std::string, int> findNumbersInLine(std::string input, int offSet) 
+std::vector<std::string> findNumbersInLine(const std::string & input)
 {
-    std::vector<std::map<std::string, int>> mapEntrys;
     std::regex pattern("(\\d+)");   // matches numbers in a string
-
     std::sregex_token_iterator it (input.begin(), input.end(), pattern);
     std::sregex_token_iterator end;
-    std::vector<std::string> foo(it, end);
+    std::vector<std::string> numbers(it, end);
+    return numbers;
+}
 
-    mapEntrys.reserve(foo.size());
-    for (auto f: foo)
+std::map<std::string, int> FOOBAR(std::string input, int offSet) 
+{
+    std::vector<std::string> numbers = findNumbersInLine(input);
+    // mapEntrys.reserve(numbers.size());
+    std::map<std::string, int> mapLine; // Map for this one line of the file
+    for (auto n: numbers)
     {
-        std::map<std::string, int> mapEntry;
-        cout << f << endl;
+        // std::map<std::string, int> mapEntry;
+        cout << n << endl;
         // Get the position of those sub string
-        size_t pos = input.find(f);
-        cout << "found at: " << pos << " - " << pos + f.size() -1 << endl;
+        size_t pos = input.find(n);
+        cout << "found at: " << pos << " - " << pos + n.size() -1 << endl;
         // Turn that possition into a string map key
         std::string mapKey = "";
         int posInt = static_cast<int>(pos);
-        for(int i = pos; i < pos + f.size(); i++)
+        for(int i = posInt; i < posInt + n.size(); i++)
         {
-            mapKey += std::to_string(i) + ",";
+            mapKey += std::to_string(i + offSet) + ",";
         }
         cout << "This is the mapKey: " << mapKey << endl;
-        mapEntry[mapKey] = stoi(f);
-        cout << "Trying to push back" << endl;
-        mapEntrys.push_back(mapEntry);
+        mapLine[mapKey] = stoi(n);
     }
-
+    // Now that I have the map entries, I want to add them to the master map
+    return mapLine;
 }
 
 int main(int argc, char* argv[])
@@ -47,9 +50,16 @@ int main(int argc, char* argv[])
     while(std::getline (MyReadFile, line_input))
     {
         int size = line_input.size();
-        cout << line_input << "size" << size << endl;
-        findNumbersInLine(line_input, offSet);
+        cout << "size: " << size << endl;
+        cout << line_input << endl;
+        // std::vector<std::string> numbers = findNumbersInLine(line_input);
+        // for (auto n: numbers)
+        // {
+            // cout << n << endl;
+        // }
+        FOOBAR(line_input, offSet);
         offSet += line_input.size();
+        return 0;
     }
     return 0;
 }
